65146041dfc7a326431539fb0e9644dfbf86d1ad
Riley Drellishak <rileydrellishak@gmail.com>
2026-08-09 12:29:15 -0500

    Rename files to SnowmanGame and SnowmanGameTest

--- backend/src/main/java/com/rileydrellishak/SnowmanGame.java
+++ backend/src/main/java/com/rileydrellishak/SnowmanGame.java
@@ -3,7 +3,7 @@ package com.rileydrellishak;
 /**
  * Hello world!
  */
-public class App {
+public class SnowmanGame {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
--- backend/src/test/java/com/rileydrellishak/SnowmanGameTest.java
+++ backend/src/test/java/com/rileydrellishak/SnowmanGameTest.java
@@ -4,14 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
+public class SnowmanGameTest {
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
